a7d44519d7331d6c04efe1a04276487db15070d1
florestomas <floresmancillatomas@gmail.com>
2023-11-02 23:34:05 -0300

    2/11

--- Proyecto/main/main.cpp
+++ Proyecto/main/main.cpp
@@ -1,4 +1,23 @@
+#include "archivos.h"
+#include "libreria.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
 int main(int argc, char *argv[]) {
-    
+
+    string nombreArchivo = "iriHorarioGYM.csv";
+
+    ifstream archivo(nombreArchivo);
+
+    if (!archivo.is_open()) {
+        cout << "No se pudo abrir el archivo " << nombreArchivo << endl;
+        return 1;
+    }
+
     return 0;
 }
