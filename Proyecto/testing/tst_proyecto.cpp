1f2d75949f58c4a5b8ed00fc0110a1acc68941b3
solgomez11 <143212224+solgomez11@users.noreply.github.com>
2023-11-16 21:23:58 -0300

    testing

--- Proyecto/testing/tst_proyecto.cpp
+++ Proyecto/testing/tst_proyecto.cpp
@@ -1,6 +1,36 @@
 #include <catch2/catch.hpp>
+#include <libreria.h>
+#include <archivos.h>
 
-TEST_CASE("My first test with Catch2", "[fancy]")
+
+TEST_CASE("funciones(correctamente)", "[correspondencia]")
 {
-    REQUIRE(0 == 0);
+    SECTION("correspondencia verdadera")
+    {
+        clase clases;
+        clases.Turnos=new Turno();//me aseguro que turnos se inicialize correctamente
+        clases.Turnos->idclas=10;
+        int m=3;
+        claseG *aux=new claseG[3];
+
+        bool resultado=correspondencia(clases,m,aux);
+        REQUIRE(resultado==true);
+        delete []aux;
+        delete clases.Turnos;
+    }
+    SECTION("correspondencia falsa")
+    {
+        clase clases;
+        clases.Turnos = new Turno(); // me aseguro que turnos se inicialize correctamente
+        clases.Turnos->idclas = 99;   // Cambié el idclas para que no coincida con el idclas en aux
+        int m = 3;
+        claseG *aux = new claseG[3];
+
+        bool resultado = correspondencia(clases, m, aux);
+        REQUIRE(resultado == false);
+
+        delete[] aux;
+        delete clases.Turnos;
+    }
 }
+
