274556a43f4f35511c4d41165c90893810a5ba3c
julilopezk <143841742+bisterflow1@users.noreply.github.com>
2023-11-23 22:00:51 -0300

    BuscarAsisCliente

--- Proyecto/libreria/gimnasio.cpp
+++ Proyecto/libreria/gimnasio.cpp
@@ -31,42 +31,14 @@ Inscripcion* resizeInscripcion(Asistencia* AsistenciaClientes, u_int cantinscrip
     }
     return nullptr;
 }
-eCliente BuscarIdCliente(ClientesGYM *Clientes, u_int idCliente, u_int CantClientes, ClientesGYM &cliente)
-{
-    ClientesGYM *aux = Clientes, *ultimo = (Clientes) + CantClientes - 1;
-    while(true) {
-        if (aux->idCliente == idCliente ){
-            cliente = *aux;
-            return eCliente :: ExitoCliente;
-        }
-        if (aux == ultimo)
-            break;
-        aux++;
-    }
-    return eCliente :: ErrIdClienteinx;
-}
 
-eClase BuscarIdClase(ClasesGym *Clases, u_int idClase, u_int cantClases, ClasesGym &clase)
-{
-    ClasesGym *aux = Clases, *ultimo = (Clases) + cantClases - 1;
-    while(true) {
-        if (aux->idClase == idClase ){
-            clase = *aux;
-            return eClase :: ExitoClase;
-        }
-        if (aux == ultimo)
-            break;
-        aux++;
-    }
-    return eClase :: ErrIdClaseinx;
-}
 int BuscarIdCliente(ClientesGYM *Clientes, u_int idCliente, u_int CantClientes, ClientesGYM &cliente)
 {
     ClientesGYM *aux = Clientes, *ultimo = (Clientes) + CantClientes - 1;
     while(true) {
         if (aux->idCliente == idCliente ){
             cliente = *aux;
-            return 1 ;//Exito Cliente
+            return 1;//Exito Cliente
         }
         if (aux == ultimo)
             break;
@@ -81,12 +53,82 @@ int BuscarIdClase(ClasesGym *Clases, u_int idClase, u_int cantClases, ClasesGym 
     while(true) {
         if (aux->idClase == idClase ){
             clase = *aux;
-            return 1 // exito id clase
+            return 1;// exito id clase
         }
         if (aux == ultimo)
             break;
         aux++;
     }
-    return 0 // error id clase
+    return 0; // error id clase
 }
 
+int BuscarIdClaseEnInscripciones(Asistencia AsistenciaUser, unsigned int idClas){
+
+    int i=0;
+
+    while(true) {
+        if (AsistenciaUser.CursosInscriptos->idCurso == idClas ){
+            return -1;
+        }
+        if ( AsistenciaUser.cantInscriptos == i)
+            break;
+        i++;
+    }
+    return 1;
+}
+eAsistencia BuscarAsisCliente(u_int idClase, u_int idCliente,  Asistencia *&AsistenciaClientes, Asistencia &AsistenciaCliente, u_int &CantAsistencias)
+{
+    int exito1,;
+    if(CantAsistencias == 0){
+        AsistenciaClientes = resizeClientes(AsistenciaClientes, CantAsistencias, CantAsistencias+1);
+
+        /*if(exito1 == 1){
+            AsistenciaClientes[CantAsistencias].idCliente = idCliente;
+            AsistenciaClientes[CantAsistencias].cantInscriptos = 1;
+            Asistencia *aux = AsistenciaClientes;
+            AsistenciaClientes[CantAsistencias].CursosInscriptos = resizeInscripcion(aux, 0, 1);
+            AsistenciaClientes[CantAsistencias].CursosInscriptos->idClase = idClase;
+            AsistenciaClientes[CantAsistencias].CursosInscriptos->fechaInscripcion = time(0);
+            AsistenciaCliente = AsistenciaClientes[CantAsistencias];
+            CantAsistencias++;
+            return 1;// ExitoCrearAsistencias;
+        }
+        else
+            return 0; // ErrEspacio;
+    }*/
+
+    else{
+        Asistencia *aux = AsistenciaClientes, *ultimo = (AsistenciaClientes) + CantAsistencias - 1;
+
+        while(true) {
+
+            if (aux->idCliente == idCliente ){
+                AsistenciaCliente = *aux;
+                return eAsistencia :: ExitoAsistencias;
+            }
+
+            if (aux == ultimo){
+                AsistenciaClientes = resizeClientes(AsistenciaClientes, CantAsistencias, CantAsistencias+1);
+
+                if(exito1 == 1){
+                    AsistenciaClientes[CantAsistencias].idCliente = idCliente;
+                    AsistenciaClientes[CantAsistencias].cantInscriptos = 1;
+                    Asistencia *aux = AsistenciaClientes;
+                    AsistenciaClientes[CantAsistencias].CursosInscriptos = resizeInscripcion(aux, 0, 1, eAsist2);
+                    AsistenciaClientes[CantAsistencias].CursosInscriptos->idClase = idClase;
+                    AsistenciaClientes[CantAsistencias].CursosInscriptos->fechaInscripcion = time(0);
+                    AsistenciaCliente = AsistenciaClientes[CantAsistencias];
+                    CantAsistencias++;
+                    return 1 // ExitoCrearAsistencias;
+                }
+
+                else
+                    return 0 // ErrEspacio;
+            }
+
+            aux++;
+        }
+    }
+
+    return 1 //ExitoAsistencias ;
+}
